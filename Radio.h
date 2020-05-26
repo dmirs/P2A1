051e5a1aa7702d0de243c55ef84e6f715202fed4
Hong <46088528+hhhong92@users.noreply.github.com>
2020-05-26 13:37:18 +0200

    Update Radio.h

--- Radio.h
+++ Radio.h
@@ -5,7 +5,7 @@ class Radio :public GeneralDevice
 {
 public:
 	Radio(std::string);
-	virtual std::string getClass() const;
+	virtual std::string getClass() const override;
 	virtual std::string toString() const override;
 	virtual Radio& operator++ ()override;
 	virtual Radio& operator--()override;
