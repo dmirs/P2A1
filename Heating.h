d190620dc67ba04c4b63a49eb7b6cfade840d4f5
Hong <46088528+hhhong92@users.noreply.github.com>
2020-05-26 13:37:04 +0200

    Update Heating.h

--- Heating.h
+++ Heating.h
@@ -5,7 +5,7 @@ class Heating :public GeneralDevice
 {
 public:
 	Heating(std::string);
-	virtual std::string getClass() const;
+	virtual std::string getClass() const override;
 	virtual std::string toString() const override;
 	virtual Heating& operator++ () override;
 	virtual Heating& operator--() override;
