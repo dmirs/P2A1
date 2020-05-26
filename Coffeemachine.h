e741cc9aae257d3887f25cc5014fce4325ce25b2
Hong <46088528+hhhong92@users.noreply.github.com>
2020-05-26 13:36:51 +0200

    Update Coffeemachine.h

--- Coffeemachine.h
+++ Coffeemachine.h
@@ -5,7 +5,7 @@ class Coffeemachine : public GeneralDevice
 {
 public:
 	Coffeemachine(std::string);
-	virtual std::string getClass() const;
+	virtual std::string getClass() const override;
 	virtual std::string toString() const override;
 	virtual Coffeemachine& operator++ ()override;
 	virtual Coffeemachine& operator--() override;
